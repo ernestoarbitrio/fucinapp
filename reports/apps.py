04d333ec5cbd7d2df7ad4d30d54cc9068712d8c0
Ernesto Arbitrio <ernesto.arbitrio@gmail.com>
2026-04-14 23:19:05 +0200

    crons

--- reports/apps.py
+++ reports/apps.py
@@ -2,4 +2,14 @@ from django.apps import AppConfig
 
 
 class ReportsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "reports"
+
+    def ready(self):
+        import os
+
+        # Only start scheduler in the main process (not in manage.py commands)
+        if os.environ.get("RUN_MAIN") or os.environ.get("GUNICORN_RUNNING"):
+            from reports.scheduler import start
+
+            start()
